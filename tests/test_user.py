5e571e24709ed8e01578e75451fb4bd2b5c1982a
vxe35 <78718233+vxe35@users.noreply.github.com>
2026-03-30 13:55:04 +1100

    Add unit tests

---
+++ tests/test_user.py
@@ -0,0 +1,14 @@
+import pytest
+from fastapi.testclient import TestClient
+
+
+def test_health_check():
+    """Test health check endpoint"""
+    # TODO: Implement test
+    assert True
+
+
+def test_create_user():
+    """Test user creation"""
+    # TODO: Implement test
+    assert True
